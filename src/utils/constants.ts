b0ab9db98879b613799ba5565dc3e26fbc827529
devJovanni <dev@jovanni.services>
2021-12-14 03:13:32 -0800

    removed all folders and files

--- src/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-export const LOGO_IMAGE = "/images/Logo.png";
-export const LIST_IMAGE = "/images/List.png";
-export const API_URL =
-  "https://s0nshulo19.execute-api.us-east-1.amazonaws.com/default/code-challenge";
